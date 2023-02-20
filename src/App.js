97b67b2cc27e4619b3f7b4af575a7372757ca1ab
yunhwan <sky10240107@naver.com>
2023-02-21 00:24:33 +0900

    fix: 더미데이터대신 API로 글 정보 생성하게  변경

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "박윤환",
-//     content: "하이 2",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "김소정",
-//     content: "하이 3",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "홍길동",
-//     content: "하이 4",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+//
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  //API를 이용하여 글을 받아오는 함수
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    console.log(res);
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_data: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,7 +1,6 @@
 import DiaryItem from "./DiaryItem";
 
 const DiaryList = ({ onEdit, onRemove, diaryList }) => {
-  console.log(diaryList);
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
